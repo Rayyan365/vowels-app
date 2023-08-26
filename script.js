b5ab456ff06e3d8deb14c3f61ba1a9cb16b0d3f1
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-26 13:39:39 -0700

    count-vowels assignment

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+function countVowels(word) {
+    let vowelCount = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+    for (let i = 0; i < word.length; i++) {
+      if (vowels.includes(word[i].toLowerCase())) {
+        vowelCount++;
+      }
+    }
+  
+    return vowelCount;
+  }
+  
+  const word = "fikrcamp";
+  const numberOfVowels = countVowels(word);
+  console.log(numberOfVowels);
